fd3ecc76ff884c3ec8255509c93772a6889022ca
canyourememberme <stuartimcintosh@Stuarts-MacBook-Pro.local>
2015-07-30 12:05:14 +0100

    More Facbook login stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,5 +39,58 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
-})
 
+  var fbLogged = new Parse.Promise();
+    
+  var fbLoginSuccess = function(response) {
+    if (!response.authResponse){
+      fbLoginError("Cannot find the authResponse");
+      return;
+    }
+    var expDate = new Date(
+      new Date().getTime() + response.authResponse.expiresIn * 1000
+    ).toISOString();
+
+    var authData = {
+      id: String(response.authResponse.userID),
+      access_token: response.authResponse.accessToken,
+      expiration_date: expDate
+    }
+    fbLogged.resolve(authData);
+    console.log(response);
+  };
+
+  var fbLoginError = function(error){
+    fbLogged.reject(error);
+  };
+
+  $scope.fbLogin = function() {
+    console.log('Login');
+    if (!window.cordova) {
+      facebookConnectPlugin.browserInit('1129125900435568');
+    }
+    facebookConnectPlugin.login(['email'], fbLoginSuccess, fbLoginError);
+  
+    fbLogged.then( function(authData) {
+      console.log('Promised');
+      return Parse.FacebookUtils.logIn(authData);
+    })
+    .then( function(userObject) {
+      facebookConnectPlugin.api('/me', null, 
+        function(response) {
+          console.log(response);
+          userObject.set('name', response.name);
+          userObject.set('email', response.email);
+          userObject.save();
+        },
+        function(error) {
+          console.log(error);
+        }
+      );
+      $scope.modal.hide();
+    }, function(error) {
+      console.log(error);
+    });
+  };
+
+})
